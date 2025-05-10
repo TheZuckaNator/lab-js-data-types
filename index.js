1097a5f5f35682ee46e8a6418960097a634933cb
AMGI <amgi@mac.home>
2025-05-10 19:48:47 +0200

    pushes lab

--- index.js
+++ index.js
@@ -1,76 +1,126 @@
-/*******************************************
-    Iteration 1.1 | Tongue Twister
-*******************************************/
-const s1 = "Fred";
-const s2 = "fed";
-const s3 = "Ted";
-const s4 = "bread";
-const s5 = "and";
+//strings
+// let const and var
+// const is always a constant meaning it never changes
+// let is a variable that can be reasigned
+const petName = "Ragnar's favorite toy";
+const ownerName = `Joshua`;
+const thePetLength = petName.length;
+// console.log("hello", thePetLength);
 
-// Concatenate the string variables into one new string
+//with variable names in JS, we use the 'camelCase' notation
+//other naming conventions
+// PascalCase
+// kebab-case-like-this
+// snake_case_is_like_this
 
+//number variables
+const petAge = "4";
+const decimal = 4;
+//remember numbers do not have a length property
+// console.log(decimal.length);
+const emptyString = 0;
+//booleans
+const isAGoodBoy = false;
+// console.log(emptyString == false);
 
-// Print out the concatenated string
+const users = ["Rishi", "Ragnar", "Joshua"];
+//Math object
+//random number between zero and one
+const randomNumber = Math.random();
+const decialNumber = 2.51;
+const roundedNumber = Math.round(2.4999999999);
+const ceilNumber = Math.ceil(3.00000001);
+const floorNumber = Math.floor(7.9999999999999);
+const smallestNumber = Math.min(2, 333, 5000, 2.1);
+const largestNumber = Math.max(1, 4, 500, 10000);
+// console.log(largestNumber);
 
+//string methods
+const dogName = "ragnar";
+//index notation
+const firstLetter = dogName[1];
+//toUpperCase()
+const capitalName = dogName[0].toUpperCase() + dogName.slice(1);
 
+//mathematical operations
 
+//raised to power
+const num = 3 ** 3;
+//modulo operator
+const isEven = 33 % 2;
+const isTrue = true;
+// OR operator is ||
+// console.log(!isTrue && num === 27);
 
-/*******************************************
-    Iteration 1.2 | Camel Tail
-*******************************************/
-const part1 = "java";
-const part2 = "script";
+//loops
+const user = "Joshua";
+let kebabCase = "";
+//for loop needs three arguments...
+//first is the starting value, second is until when to run the loop, third is how much to increment the loop
+for (let i = 0; i < user.length; i++) {
+  //   console.log(user[i] + "-");
+  if (i < user.length - 1) {
+    kebabCase += user[i] + "-";
+  } else {
+    kebabCase += user[i];
+  }
+}
 
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const newName = "Roberto";
+let reversedName = "";
+// console.log(kebabCase);
+//for loop in reverse
+for (let i = newName.length - 1; i >= 0; i--) {
+  //   console.log(newName[i]);
+  reversedName += newName[i];
+}
+// console.log(reversedName);
 
+//FIZZ BUZZ Challenge :)
+//write a loop that counts to 100 from 0
+//if the number is divisible by 3 then you should log 'FIZZ'
+//if the number is divisible by 5 then you should log 'BUZZ'
+//if the number is divisible by 3 and 5 then you should log 'FIZZ BUZZ'
+//else you should just log the number
 
-// Print the cameLtaiL-formatted string
+for (let i = 1; i <= 30; i++) {
+  if (i % 15 === 0) {
+    console.log("Fizz Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
 
+// //with Switch statement
+for (let j = 0; j <= 30; j++) {
+  switch (true) {
+    case j % 15 === 0:
+      console.log("FUZZ BUZZ");
+      break;
+    case j % 3 === 0:
+      console.log("FIZZ");
+      break;
+    case j % 5 === 0:
+      console.log("BUZZ");
+      break;
 
+    default:
+      console.log(j);
+      break;
+  }
+}
 
+let test = "hello";
+let test2 = "world";
+const concatenated = test + " " + test2;
+const withTemplateLiterals = `hi ${test} ${test2} `;
+console.log(withTemplateLiterals);
 
-/*******************************************
-    Iteration 2.1 | Calculate Tip
-*******************************************/
-const billTotal = 84;
-
-// Calculate the tip (15% of the bill total)
-
-
-// Print out the tipAmount
-
-
-
-
-/*******************************************
-    Iteration 2.2 | Generate Random Number
-*******************************************/
-
-// Generate a random integer between 1 and 10 (inclusive)
-
-
-// Print the generated random number
-
-
-
-/*******************************************
-    Iteration 3.1 | Booleans
-*******************************************/
-
-const a = true;
-const b = false;
-
-// Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
-
-const expression2 = a || b;
-
-const expression3 = !a && b;
-
-const expression4 = !(a && b);
-
-const expression5 = !a || !b;
-
-const expression6 = !(a || b);
-
-const expression7 = a && a;
+//cal a tip
+const total = 84;
+const tip = total * 0.15;
+console.log(total + tip);
